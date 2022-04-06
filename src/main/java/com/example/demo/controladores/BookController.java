116d0bc5556ed6c4fa901599d5cf28b1f51feb7f
mfcg15 <fcardenas1504@gmail.com>
2022-04-05 19:50:42 -0500

    Renderizar Libros

---
+++ src/main/java/com/example/demo/controladores/BookController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.modelos.Book;
+import com.example.demo.servicios.BookService;
+
+
+@Controller
+public class BookController 
+{
+	
+	private final BookService bookService;
+	
+	public BookController(BookService bookService) 
+	{
+		this.bookService = bookService;
+	}
+
+	@RequestMapping("/books/{id}")
+	public String show (Model model,@PathVariable("id") Long id)
+	{
+		Book book = bookService.findBook(id);
+		
+		model.addAttribute("title",book.getTitle());
+		model.addAttribute("description",book.getDescription());
+		model.addAttribute("language",book.getLanguage());
+		model.addAttribute("pages",book.getNumberOfPages());
+		
+		return "show.jsp";
+	}
+
+}
